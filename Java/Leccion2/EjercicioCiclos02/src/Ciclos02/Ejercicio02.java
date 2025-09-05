202245d1c2ffa919197c9458f0a9a43cc67ea568
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-05 15:49:02 -0300

    Terminamos la Leccion2 de Java
    Terminamos la Leccion2 de Java, como el tutor nos dijo cómo realmente tiene que estar en el repositorio, yo en local (mi pc), la tengo igual que este repositorio. Sino lo contrario, seria un repositorio desaprobado

---
+++ Java/Leccion2/EjercicioCiclos02/src/Ciclos02/Ejercicio02.java
@@ -0,0 +1,23 @@
+package Ciclos02;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un numero: ");
+        var numero = Integer.parseInt(entrada.nextLine());
+        while(numero != 0){
+            if(numero > 0){
+                System.out.println("El número "+numero+" es POSITIVO");
+            }
+            else{
+                System.out.println("El número "+numero+" es NEGATIVO");
+            }
+            System.out.println("Digite otro numero: ");
+            numero = Integer.parseInt(entrada.nextLine());
+        }
+        System.out.println("El número "+numero+" finaliza el programa");
+    }
+
+}
