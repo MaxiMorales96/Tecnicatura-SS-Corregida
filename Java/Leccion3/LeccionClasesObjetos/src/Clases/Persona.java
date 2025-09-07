c9a77f0a5cdf3bd117df897d03fd49ce464e61ab
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-06 23:25:47 -0300

    Arregle ejercicios en Leccion 3 en Clases

--- Java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
+++ Java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
@@ -1,19 +1,17 @@
-/*CLASE2: Tema: Ejercicios con ciclos y Crear Clases POO parte 0(20-08)
- Punto: 2.6-Crear Clases en Java*/
- /*CLASE 4: Clases y Objetos Poo parte 2 Tarea 
-  Punto 4.1-Metodos*/
+/*CLASE 4:Clases y Objetos Poo parte 2 Tarea
+4.1- Metodos (vIDEO1)
+4.2 Creacion de un Objeto(ViDEO 2)*/
 package Clases;
 
 public class Persona {
 
-    //Atributos de la clases (Caracteristicas)
+    //Atributos de la clase(caracteristicas)
     String nombre;
     String apellido;
 
-    //Métodos de la clase (Acciones)
+    // Metodo de la clase (Acciones)
     public void obtenerInformacion() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
-
     }
 }
